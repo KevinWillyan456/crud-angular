86870a2d2e7b28b0b392a14c3731e6048a8b7b9c
Kevin Souza <kevinwillyansouza@gmail.com>
2024-09-16 18:08:05 -0400

    feat: criação da tabela para mostrar os cursos

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import { Course } from '../models/course';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './courses.component.html',
-  styleUrl: './courses.component.scss'
+  styleUrl: './courses.component.scss',
 })
 export class CoursesComponent {
-
+  courses = new MatTableDataSource<Course>([
+    {
+      _id: '1',
+      name: 'Angular',
+      category: 'Frontend',
+    },
+    {
+      _id: '2',
+      name: 'React',
+      category: 'Frontend',
+    },
+    {
+      _id: '3',
+      name: 'Vue',
+      category: 'Frontend',
+    },
+    {
+      _id: '4',
+      name: 'Express',
+      category: 'Backend',
+    },
+    {
+      _id: '5',
+      name: 'Django',
+      category: 'Backend',
+    },
+    {
+      _id: '6',
+      name: 'Flask',
+      category: 'Backend',
+    },
+  ]);
+  displayedColumns = ['name', 'category'];
 }
