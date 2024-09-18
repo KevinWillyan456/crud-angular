59ba9e03d9a549408962a8990edacef55a294b56
Kevin Souza <kevinwillyansouza@gmail.com>
2024-09-17 21:57:13 -0400

    feat: criação do spinner de carregamento

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Course } from '../models/course';
-import { first, tap } from 'rxjs/operators';
+import { delay, first, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -14,6 +14,7 @@ export class CoursesService {
   list() {
     return this.httpClient.get<Course[]>(this.API).pipe(
       first(),
+      delay(2000),
       tap((courses) => console.log(courses))
     );
   }
