608c5bc5cbae5d449d833ff8f705124ac3e6adeb
Kevin Souza <kevinwillyansouza@gmail.com>
2024-09-17 21:34:00 -0400

    feat: criação da chamada HTTP GET

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,45 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Course } from '../models/course';
+import { first, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CoursesService {
+  private readonly API = '/assets/courses.json';
+
   constructor(private httpClient: HttpClient) {}
 
-  list(): Course[] {
-    return [
-      {
-        _id: '1',
-        name: 'Angular',
-        category: 'Frontend',
-      },
-      {
-        _id: '2',
-        name: 'React',
-        category: 'Frontend',
-      },
-      {
-        _id: '3',
-        name: 'Vue',
-        category: 'Frontend',
-      },
-      {
-        _id: '4',
-        name: 'Express',
-        category: 'Backend',
-      },
-      {
-        _id: '5',
-        name: 'Django',
-        category: 'Backend',
-      },
-      {
-        _id: '6',
-        name: 'Flask',
-        category: 'Backend',
-      },
-    ];
+  list() {
+    return this.httpClient.get<Course[]>(this.API).pipe(
+      first(),
+      tap((courses) => console.log(courses))
+    );
   }
 }
